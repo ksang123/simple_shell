421502b8bee0ab0b38e01fd4096554abc4334c83
ksang123 <etaizilberman@gmail.com>
2024-03-09 15:56:56 +0200

    moving to linux only

--- main.c
+++ main.c
@@ -1,24 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_terminal(char* user, char* machine);
-void terminal();
+void terminal(char* user, char* machine);
 
 int main() {
-    #if defined(_WIN64)
-        system(".\\ansicon\\x64\\ansicon.exe > NUL 2>&1");
-    #endif
-    #if defined(_WIN32)
-    system(".\\ansicon\\x86\\ansicon.exe > NUL 2>&1");
-    #endif
-    terminal();
+    system("clear");
+    terminal("user", "machine");
     return 0;
 }
 
-void terminal() {
-    print_terminal("user", "machine");
-}
-
-void print_terminal(char* user, char* machine) {
-    printf("\033[1;34m%s@m%s-\033[32m[path]\033[1;34m $ \033[0m", user, machine);
+void terminal(char* user, char* machine) {
+    char* command;
+    printf("\033[1;34m%s@%s-\033[32m[path]\033[1;34m $ \033[0m\n", user, machine);
 }
